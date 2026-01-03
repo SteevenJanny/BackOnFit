4d232165d842de299ef88d1bfe87d6ac6b15e250
steeven-janny <sj777@orange.fr>
2026-01-03 16:40:29 +0100

    Update some deps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,33 +1,33 @@
 import type {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.backonfit.app',
-  appName: 'BackOnFit',
-  webDir: 'www',
-  plugins: {
-    LocalNotifications: {
-      iconColor: "#0d9388",
+    appId: 'com.backonfit.app',
+    appName: 'BackOnFit',
+    webDir: 'www',
+    plugins: {
+        LocalNotifications: {
+            iconColor: "#0d9388",
+        },
+        CapacitorSQLite: {
+            iosDatabaseLocation: 'Library/CapacitorDatabase',
+            iosIsEncryption: true,
+            iosKeychainPrefix: 'angular-sqlite-app-starter',
+            iosBiometric: {
+                biometricAuth: false,
+                biometricTitle: "Biometric login for capacitor sqlite"
+            },
+            androidIsEncryption: true,
+            androidBiometric: {
+                biometricAuth: false,
+                biometricTitle: "Biometric login for capacitor sqlite",
+                biometricSubTitle: "Log in using your biometric"
+            },
+            electronIsEncryption: true,
+            electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+            electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+            electronLinuxLocation: "Databases"
+        },
     },
-    CapacitorSQLite: {
-      iosDatabaseLocation: 'Library/CapacitorDatabase',
-      iosIsEncryption: true,
-      iosKeychainPrefix: 'angular-sqlite-app-starter',
-      iosBiometric: {
-        biometricAuth: false,
-        biometricTitle: "Biometric login for capacitor sqlite"
-      },
-      androidIsEncryption: true,
-      androidBiometric: {
-        biometricAuth: false,
-        biometricTitle: "Biometric login for capacitor sqlite",
-        biometricSubTitle: "Log in using your biometric"
-      },
-      electronIsEncryption: true,
-      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
-      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
-      electronLinuxLocation: "Databases"
-    }
-  },
 };
 
 export default config;
